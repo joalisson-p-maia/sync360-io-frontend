8821b6b85ffe16f6bfb8fa07caca5c7e701c9b9c
joalisson-p-maia <namibo654@gmail.com>
2025-07-10 00:17:44 -0300

    feat: created five methods to create, list all, return one, update, and delete user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListAllUsersType } from '../types/listAllUsersType';
+import { GetUserByIdType } from '../types/getUserByIdType';
+import { MessageStatusType } from '../types/messageStatusType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(
+    private readonly http: HttpClient
+  ) { }
 
-  constructor() { }
+  apiUrl: string = "http://localhost:8000/api/user/";
+
+  listAllUsers(): Observable<ListAllUsersType>{
+    return this.http.get<ListAllUsersType>(this.apiUrl+'list_all_users');
+  }
+
+  getUserById(userId: string | null): Observable<GetUserByIdType>{
+    return this.http.get<GetUserByIdType>(this.apiUrl+'get_user_by_id/' + userId);
+  }
+
+  createUser(data: any): Observable<MessageStatusType>{
+    return this.http.post<MessageStatusType>(this.apiUrl+'create_user', data);
+  }
+
+  updateUser(userId: string | null,data: any): Observable<MessageStatusType>{
+    return this.http.post<MessageStatusType>(this.apiUrl + 'update_user_by_id/' + userId, data);
+  }
+
+  deleteUser(userId: string | null): Observable<MessageStatusType>{
+    return this.http.delete<MessageStatusType>(this.apiUrl + 'delete_user_by_id/' + userId);
+  }
 }

23a24e7beb5493a5fded0b2cb89162448d152f72
joalisson-p-maia <namibo654@gmail.com>
2025-07-10 00:16:39 -0300

    feat: created more routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ViewUserComponent } from './pages/view-user/view-user.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'usuario/:id',
+        component: ViewUserComponent
+    },
+    {
+        path: 'criar-usuario',
+        component: CreateUserComponent
+    },
+    {
+        path: 'atualizar-usuario/:id',
+        component: UpdateUserComponent
     },
     {
         path: '**',
